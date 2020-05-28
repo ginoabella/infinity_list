350ff3460cc71b40332a766e484fd25be2c836df
gino abella <gino@ginos-MacBook-Air.local>
2020-05-28 13:16:52 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:infinit_list/src/app.dart';
+
+void main() {
+  runApp(App());
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:infinit_list/src/bloc/post_bloc.dart';
+import 'package:infinit_list/src/screens/home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Infinite Scroll',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Posts'),
+        ),
+        body: BlocProvider(
+          create: (context) =>
+              PostBloc(httpClient: http.Client())..add(Fetch()),
+          child: HomePage(),
+        ),
+      ),
+    );
+  }
+}
